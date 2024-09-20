d65d112c3d7c57f56744abcd1c078b73781dafe7
datx24 <xuandat475@gmail.com>
2024-09-20 15:38:33 +0700

    làm bài tính tổng

--- app/src/main/java/dat/nguyenxuan/viduhello/MainActivity.java
+++ app/src/main/java/dat/nguyenxuan/viduhello/MainActivity.java
@@ -1,6 +1,9 @@
 package dat.nguyenxuan.viduhello;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public  void  SayHello(View v) {
+        //Tìm điều khiển
+        EditText soThuNhat = findViewById(R.id.edtNum);
+        EditText soThuHai = findViewById(R.id.edtNum1);
+        //Lấy dữ liệu. getter
+        String s1 = soThuNhat.getText().toString();
+        String s2 = soThuHai.getText().toString();
+        //Chuyển kiểu
+        double num1 = Double.parseDouble(s1);
+        double num2 = Double.parseDouble(s2);
+        //Tinh toán
+        double tong = num1 + num2;
+
+        //Chuẩn bị đề xuất
+        String chuoiXuat = "Tổng là: " + String.valueOf(tong);
+
+        Toast.makeText(this,"Bạn vừa chạm tôi!",Toast.LENGTH_SHORT).show();
     }
 }
