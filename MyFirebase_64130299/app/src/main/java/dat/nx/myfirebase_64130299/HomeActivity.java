4cf948f6c7c51ed1e5afaa369d5d52ab132a2cc8
datx24 <xuandat475@gmail.com>
2024-11-18 23:32:46 +0700

    cài đặt home activity

--- MyFirebase_64130299/app/src/main/java/dat/nx/myfirebase_64130299/HomeActivity.java
+++ MyFirebase_64130299/app/src/main/java/dat/nx/myfirebase_64130299/HomeActivity.java
@@ -1,24 +1,47 @@
 package dat.nx.myfirebase_64130299;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private TextView welcomeMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
+        welcomeMessage = findViewById(R.id.welcomeMessage);
+
+        String userId = getIntent().getStringExtra("USER_ID");
+        if (userId != null) {
+            loadUserData(userId);
+        }
+    }
+
+    private void loadUserData(String userId) {
+        mDatabase.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String email = snapshot.child("email").getValue(String.class);
+                welcomeMessage.setText("Welcome, " + email);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                welcomeMessage.setText("Failed to load user data.");
+            }
         });
     }
 }
