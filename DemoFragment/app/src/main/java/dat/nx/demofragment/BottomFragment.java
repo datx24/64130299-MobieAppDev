26d94ca082b9d0d0c33034225c3e96a40aa4f8d3
datx24 <xuandat475@gmail.com>
2024-11-20 00:50:03 +0700

    xử lý đổi màu khi nhấn nút trong bottom

--- DemoFragment/app/src/main/java/dat/nx/demofragment/BottomFragment.java
+++ DemoFragment/app/src/main/java/dat/nx/demofragment/BottomFragment.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
 import dat.nx.demofragment.R;
 
 public class BottomFragment extends Fragment {
@@ -17,7 +20,29 @@ public class BottomFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate layout for this fragment
         View view = inflater.inflate(R.layout.fragment_bottom, container, false);
+        // Ánh xạ các nút
+        MaterialButton buttonBlue = view.findViewById(R.id.button_one);
+        MaterialButton buttonGreen = view.findViewById(R.id.button_two);
+        MaterialButton buttonRed = view.findViewById(R.id.button_three);
 
+        // Thiết lập sự kiện click cho các nút
+        buttonBlue.setOnClickListener(v -> {
+            if (getActivity() instanceof TopFragment.OnColorChangeListener) {
+                ((TopFragment.OnColorChangeListener) getActivity()).onColorChange(0x3F51B5);
+            }
+        });
+
+        buttonGreen.setOnClickListener(v -> {
+            if (getActivity() instanceof TopFragment.OnColorChangeListener) {
+                ((TopFragment.OnColorChangeListener) getActivity()).onColorChange(0xFF03DAC5); // Green
+            }
+        });
+
+        buttonRed.setOnClickListener(v -> {
+            if (getActivity() instanceof TopFragment.OnColorChangeListener) {
+                ((TopFragment.OnColorChangeListener) getActivity()).onColorChange(0xFFFF5722); // Red
+            }
+        });
         return view;
     }
 }
