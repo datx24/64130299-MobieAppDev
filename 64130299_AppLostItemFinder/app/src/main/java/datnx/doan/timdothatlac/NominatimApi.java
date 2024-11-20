555a2f3c41185c01e6e6b0fb15c7ee6397da0ce4
datx24 <xuandat475@gmail.com>
2024-11-20 22:38:53 +0700

    tạo Api client để gửi yêu cầu lấy vị trí hiện tại

---
+++ 64130299_AppLostItemFinder/app/src/main/java/datnx/doan/timdothatlac/NominatimApi.java
@@ -0,0 +1,15 @@
+package datnx.doan.timdothatlac;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NominatimApi {
+    @GET("reverse")
+    Call<LocationResponse> getLocation(
+            @Query("lat") double latitude,
+            @Query("lon") double longitude,
+            @Query("format") String format
+    );
+}
+
