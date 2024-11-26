b5621e8fd7069fc3a14d4c694c7cdd8a3a4e2741
datx24 <xuandat475@gmail.com>
2024-11-26 14:59:38 +0700

    cài đặt để ItemDetailActivity nhận được dữ liệu

--- 64130299_AppLostItemFinder/app/src/main/java/datnx/doan/timdothatlac/ItemDetailActivity.java
+++ 64130299_AppLostItemFinder/app/src/main/java/datnx/doan/timdothatlac/ItemDetailActivity.java
@@ -3,12 +3,16 @@ package datnx.doan.timdothatlac;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
@@ -17,11 +21,17 @@ public class ItemDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_item_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //Nhận dữ liệu từ intent
+        String itemName = getIntent().getStringExtra("name");
+        String itemImageUrl = getIntent().getStringExtra("image_url");
+
+        //Hiển thị dữ liệu lên giao diện
+        ImageView imageView = findViewById(R.id.imgItem);
+        TextView nameTextView = findViewById(R.id.tvItemName);
+
+        nameTextView.setText(itemName);
+        Picasso.get().load(itemImageUrl).into(imageView);
     }
 
     // Phương thức được gọi khi nhấn vào nút
