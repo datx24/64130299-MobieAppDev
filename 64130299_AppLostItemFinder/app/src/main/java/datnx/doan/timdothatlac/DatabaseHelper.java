f223adb3688a2479293edfa55b39124dc2fe380f
datx24 <xuandat475@gmail.com>
2024-11-28 21:51:28 +0700

    Tạo cơ sở dữ liệu SQLite

---
+++ 64130299_AppLostItemFinder/app/src/main/java/datnx/doan/timdothatlac/DatabaseHelper.java
@@ -0,0 +1,53 @@
+package datnx.doan.timdothatlac;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    //Tên csdl
+    private static final String DATABASE_NAME = "LostItems.db";
+
+    //Phiên bản csdl
+    private static final int DATABASE_VERSION = 1;
+
+    //Tạo bảng và cột
+    public static final String TABLE_NAME = "Items";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_IMAGE = "image";
+    public static final String COLUMN_ADDRESS = "address";
+    public static final String COLUMN_LATITUDE = "latitude";
+    public static final String COLUMN_LONGITUDE = "longitude";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //Tạo bảng
+    private static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_NAME + " TEXT, " +
+                    COLUMN_IMAGE + " TEXT, " +
+                    COLUMN_ADDRESS + " TEXT, " +
+                    COLUMN_LATITUDE + " REAL, " +
+                    COLUMN_LONGITUDE + " REAL, " +
+                    COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP)";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Tạo bảng
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Xóa bảng cũ
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
