0fde15d9b0b8860545dc7454ae160444620f5c98
datx24 <xuandat475@gmail.com>
2024-12-03 21:30:52 +0700

    cài đặt ánh xạ các thành phần trên giao diện

--- SimpleMath/app/src/main/java/th/nguyenxuandat/SimpleMath/MainActivity.java
+++ SimpleMath/app/src/main/java/th/nguyenxuandat/SimpleMath/MainActivity.java
@@ -1,24 +1,24 @@
 package th.nguyenxuandat.SimpleMath;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //Ánh xạ các thành phần trên giao diện
+        EditText inputA = findViewById(R.id.inputA);
+        EditText inputB = findViewById(R.id.inputB);
+        EditText result = findViewById(R.id.editResults);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        Button btnCalculate = findViewById(R.id.btnResult);
     }
 }
