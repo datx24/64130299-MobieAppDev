eefcc84d9e13a7b732763e4d8cbb555ac642dd62
datx24 <xuandat475@gmail.com>
2024-12-04 00:51:10 +0700

    cài đặt sự kiện btn chuyển màn hình

--- ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/MainActivity.java
+++ ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/MainActivity.java
@@ -1,6 +1,8 @@
 package th.nguyenxuandat.ChuyenManHinh_Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnEnglish = findViewById(R.id.btnEnglish);
+        Button btnMath = findViewById(R.id.btnMath);
+        Button btnProgramming = findViewById(R.id.btnProgramming);
+
+        //Chuyển đến màn hình English
+        btnEnglish.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EnglishActivity.class);
+            startActivity(intent);
+        });
+
+        //Chuyển đến màn hình Math
+        btnMath.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,MathActivity.class);
+            startActivity(intent);
+        });
+
+        //Chuyển đến màn hình Programming
+        btnProgramming.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,ProgrammingActivity.class);
+            startActivity(intent);
         });
     }
 }
