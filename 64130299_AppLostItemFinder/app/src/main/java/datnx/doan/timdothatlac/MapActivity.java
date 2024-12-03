c3b2f6bbe6c5e1152ebc6267780ac4fab088df45
datx24 <xuandat475@gmail.com>
2024-12-03 14:41:28 +0700

    Nhận dữ liệu từ Intent và tạo bản đồ trong giao diện

--- 64130299_AppLostItemFinder/app/src/main/java/datnx/doan/timdothatlac/MapActivity.java
+++ 64130299_AppLostItemFinder/app/src/main/java/datnx/doan/timdothatlac/MapActivity.java
@@ -1,6 +1,10 @@
 package datnx.doan.timdothatlac;
 
+import static android.view.View.*;
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +12,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
 
 public class MapActivity extends AppCompatActivity {
@@ -19,5 +26,15 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_map);
+
+        //Nhận dữ liệu từ Intent
+        latitude = getIntent().getDoubleExtra("latitude",0);
+        longitude = getIntent().getDoubleExtra("longitude",0);
+
+        //Khởi tạo bản đồ
+        mapView = findViewById(R.id.osmMapView);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+        mapView.setMultiTouchControls(true);
     }
 }
