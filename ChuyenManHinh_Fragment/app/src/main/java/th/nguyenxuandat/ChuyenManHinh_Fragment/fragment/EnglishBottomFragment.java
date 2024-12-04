bef467169cee02d8a692e78aacf92a33f9b02598
datx24 <xuandat475@gmail.com>
2024-12-04 22:26:17 +0700

    cài đặt onCreateView lắng nghe sự kiện bấm hai nút next và prev

--- ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/fragment/EnglishBottomFragment.java
+++ ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/fragment/EnglishBottomFragment.java
@@ -7,13 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import th.nguyenxuandat.ChuyenManHinh_Fragment.R;
 public class EnglishBottomFragment extends Fragment {
+    private Button btnPrevFragment,btnNextFragment;
+    private int currentFramentIndex = 0; //Theo dõi bị trí fragment hiện tại
+
+    public EnglishBottomFragment() {
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_english_bottom, container, false);
+        View view = inflater.inflate(R.layout.fragment_english_bottom, container, false);
+
+        //Tạo các nút từ layout
+        btnPrevFragment = view.findViewById(R.id.btnPrevFragment);
+        btnNextFragment = view.findViewById(R.id.btnNextFragment);
+
+        //Thiết lập sự kiện cho nút btnNext và btnPrev
+        btnPrevFragment.setOnClickListener(v -> navigateToFragment(false));
+        btnNextFragment.setOnClickListener(v -> navigateToFragment(true));
+        
+        return view;
     }
 }
