98d30ad720d5dd84b00866bc952c25660261ed44
datx24 <xuandat475@gmail.com>
2024-12-04 23:12:38 +0700

    gắn bottom frament vào activity

--- ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/activity/MathActivity.java
+++ ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/activity/MathActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.MathBottomFragment;
 import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.MathFragment1;
 import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.MathFragment2;
 import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.MathFragment3;
@@ -19,21 +20,17 @@ public class MathActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math);
 
-        Button btnFragment1 = findViewById(R.id.btnMathFragment1);
-        Button btnFragment2 = findViewById(R.id.btnMathFragment2);
-        Button btnFragment3 = findViewById(R.id.btnMathFragment3);
 
         //Hiển thị frament 1 làm mặc định
         loadFragment(new MathFragment1());
+        loadBottomFramment();
+    }
 
-        //Chuyển đến Frament 1
-        btnFragment1.setOnClickListener(v -> loadFragment(new MathFragment1()));
-        
-        //Chuyển đến Fragment 2
-        btnFragment2.setOnClickListener(v -> loadFragment(new MathFragment2()));
-
-        //Chuyển đến Fragment 3
-        btnFragment3.setOnClickListener(v -> loadFragment(new MathFragment3()));
+    private void loadBottomFramment() {
+        // Load Bottom Fragment
+        FragmentTransaction bottomTransaction = getSupportFragmentManager().beginTransaction();
+        bottomTransaction.replace(R.id.bottomMathFragment, new MathBottomFragment());
+        bottomTransaction.commit();
     }
 
     private void loadFragment(Fragment fragment) {
@@ -42,4 +39,5 @@ public class MathActivity extends AppCompatActivity {
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
 }
