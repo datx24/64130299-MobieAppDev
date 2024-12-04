8d0b5b6093eabbb02db5994927cf6b499e1ce0ff
datx24 <xuandat475@gmail.com>
2024-12-04 22:10:34 +0700

    xóa các button

--- ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/activity/EnglishActivity.java
+++ ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/activity/EnglishActivity.java
@@ -21,24 +21,11 @@ public class EnglishActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_english);
 
-        Button btnFragment1 = findViewById(R.id.btnEnglishFragment1);
-        Button btnFragment2 = findViewById(R.id.btnEnglishFragment2);
-        Button btnFragment3 = findViewById(R.id.btnEnglishFragment3);
-
         // Hiển thị Top và Bottom Fragments
         loadTopAndBottomFragments();
 
         //Hiển thị frament 1 làm mặc định
         loadFragment(new EnglishFragment1());
-
-        //Chuyển đến Frament 1
-        btnFragment1.setOnClickListener(v -> loadFragment(new EnglishFragment1()));
-
-        //Chuyển đến Fragment 2
-        btnFragment2.setOnClickListener(v -> loadFragment(new EnglishFragment2()));
-
-        //Chuyển đến Fragment 3
-        btnFragment3.setOnClickListener(v -> loadFragment(new EnglishFragment3()));
     }
 
     private void loadFragment(Fragment fragment) {
