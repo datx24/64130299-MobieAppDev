94e8f6dc3bd2d1aa7b3ef231d61412fb8aa05a29
datx24 <xuandat475@gmail.com>
2024-12-04 22:09:12 +0700

    cài đặt hiển thị top và bottom framment

--- ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/activity/EnglishActivity.java
+++ ChuyenManHinh_Fragment/app/src/main/java/th/nguyenxuandat/ChuyenManHinh_Fragment/activity/EnglishActivity.java
@@ -7,10 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.EnglishBottomFragment;
 import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.EnglishFragment1;
 import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.EnglishFragment2;
 import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.EnglishFragment3;
 import th.nguyenxuandat.ChuyenManHinh_Fragment.R;
+import th.nguyenxuandat.ChuyenManHinh_Fragment.fragment.EnglishTopFragment;
 
 public class EnglishActivity extends AppCompatActivity {
 
@@ -22,6 +24,9 @@ public class EnglishActivity extends AppCompatActivity {
         Button btnFragment1 = findViewById(R.id.btnEnglishFragment1);
         Button btnFragment2 = findViewById(R.id.btnEnglishFragment2);
         Button btnFragment3 = findViewById(R.id.btnEnglishFragment3);
+
+        // Hiển thị Top và Bottom Fragments
+        loadTopAndBottomFragments();
 
         //Hiển thị frament 1 làm mặc định
         loadFragment(new EnglishFragment1());
@@ -41,5 +46,17 @@ public class EnglishActivity extends AppCompatActivity {
         transaction.replace(R.id.englishFragmentContainerView, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    private void loadTopAndBottomFragments() {
+        // Load Top Fragment
+        FragmentTransaction topTransaction = getSupportFragmentManager().beginTransaction();
+        topTransaction.replace(R.id.topEnglishFragment, new EnglishTopFragment());
+        topTransaction.commit();
+
+        // Load Bottom Fragment
+        FragmentTransaction bottomTransaction = getSupportFragmentManager().beginTransaction();
+        bottomTransaction.replace(R.id.bottomEnglishFragment, new EnglishBottomFragment());
+        bottomTransaction.commit();
     }
 }
