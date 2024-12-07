2b605b42f4d099f5a58fa58951414a3aee4a4a09
datx24 <xuandat475@gmail.com>
2024-12-08 01:28:43 +0700

    tạo lớp notification

---
+++ BottomNavigationBar/app/src/main/java/th/nguyenxuandat/BotNav_Recy/models/entity/Notification.java
@@ -0,0 +1,13 @@
+package th.nguyenxuandat.BotNav_Recy.models.entity;
+
+public class Notification {
+    private String message;
+
+    public Notification(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
