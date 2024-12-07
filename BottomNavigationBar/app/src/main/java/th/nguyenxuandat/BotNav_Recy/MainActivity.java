382a7fc82ce7dc92e02bb7e6c0298d0a535f0917
datx24 <xuandat475@gmail.com>
2024-12-07 22:40:23 +0700

    cài đặt load fragment khi bấm nút trong botnavbar

--- BottomNavigationBar/app/src/main/java/th/nguyenxuandat/BotNav_Recy/MainActivity.java
+++ BottomNavigationBar/app/src/main/java/th/nguyenxuandat/BotNav_Recy/MainActivity.java
@@ -1,12 +1,12 @@
 package th.nguyenxuandat.BotNav_Recy;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //Thiết lập listener cho bottomNavigationView
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home) {
+                loadFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.nav_transaction) {
+                loadFragment(new TransactionFragment());
+            } else if (item.getItemId() == R.id.nav_notification) {
+                loadFragment(new NotificationFragment());
+            } else if (item.getItemId() == R.id.nav_setting) {
+                loadFragment(new SettingFragment());
+            }
+            return true;
+        });
+
+        //Load fragment mặc định khi lần đầu mở màn hình
+        loadFragment(new HomeFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
     }
 }
