d2c2ead6c951e74cf972160e4f609b0c44161e44
datx24 <xuandat475@gmail.com>
2024-12-08 01:29:46 +0700

    cài đặt notification adapter

---
+++ BottomNavigationBar/app/src/main/java/th/nguyenxuandat/BotNav_Recy/adapter/NotificationAdapter.java
@@ -0,0 +1,50 @@
+package th.nguyenxuandat.BotNav_Recy.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import th.nguyenxuandat.BotNav_Recy.models.entity.Notification;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
+
+    private List<Notification> notificationList;
+
+    public NotificationAdapter(List<Notification> notificationList) {
+        this.notificationList = notificationList;
+    }
+
+    @NonNull
+    @Override
+    public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new NotificationViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotificationViewHolder holder, int position) {
+        Notification notification = notificationList.get(position);
+        holder.textView.setText(notification.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notificationList.size();
+    }
+
+    public static class NotificationViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public NotificationViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(android.R.id.text1);
+        }
+    }
+}
