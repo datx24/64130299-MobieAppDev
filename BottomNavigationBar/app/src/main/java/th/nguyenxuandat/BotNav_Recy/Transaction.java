60886da8fa4fd03353f5757c3368c75aba5beeeb
datx24 <xuandat475@gmail.com>
2024-12-08 00:49:52 +0700

    tạo lớp lưu thông tin hóa đơn

---
+++ BottomNavigationBar/app/src/main/java/th/nguyenxuandat/BotNav_Recy/Transaction.java
@@ -0,0 +1,20 @@
+package th.nguyenxuandat.BotNav_Recy;
+
+public class Transaction {
+    private int image;
+    private String name;
+
+    public Transaction(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
