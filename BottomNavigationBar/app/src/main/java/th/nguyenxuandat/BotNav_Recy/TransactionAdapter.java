8075ce5836f6e755df4ea9ca302d61b8701f388c
datx24 <xuandat475@gmail.com>
2024-12-08 00:51:57 +0700

    cài đặt transaction adapter

---
+++ BottomNavigationBar/app/src/main/java/th/nguyenxuandat/BotNav_Recy/TransactionAdapter.java
@@ -0,0 +1,50 @@
+package th.nguyenxuandat.BotNav_Recy;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
+
+    private List<Transaction> transactions;
+
+    public TransactionAdapter(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    @NonNull
+    @Override
+    public TransactionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.transaction_item, parent, false);
+        return new TransactionViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TransactionViewHolder holder, int position) {
+        Transaction transaction = transactions.get(position);
+        holder.imgTransaction.setImageResource(transaction.getImage());
+        holder.tvTransaction.setText(transaction.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return transactions.size();
+    }
+
+    public static class TransactionViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgTransaction;
+        TextView tvTransaction;
+
+        public TransactionViewHolder(View view) {
+            super(view);
+            imgTransaction = view.findViewById(R.id.imgTransaction);
+            tvTransaction = view.findViewById(R.id.tvTransaction);
+        }
+    }
+}
